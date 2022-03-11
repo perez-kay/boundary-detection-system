d845e8863bdaeaf0b6352aec85b54b27b63303eb
Kayla Perez <kayla.perez270@gmail.com>
2022-03-11 14:27:15 -0800

    cleaned up

--- Frame.py
+++ Frame.py
@@ -23,25 +23,7 @@ class Frame:
     def to_dict(self):
         self.img = self.img.tolist()
         self.histogram = self.histogram.tolist()
-        return self.__dict__
-
-
-    def get_intensity(self, RGB_vals):
-        """
-        Calculates the intesity for the given RGB values.
-
-        Parameters
-        ----------
-        vals : tuple
-            The RGB values, in RGB order
-
-        Returns
-        -------
-        float
-            The intensity value
-        """
-
-        return (.299*RGB_vals[0]) + (.587*RGB_vals[1]) + (.114 * RGB_vals[2])    
+        return self.__dict__  
 
     def compute_histogram(self):
         """
@@ -53,17 +35,11 @@ class Frame:
         np.array
             The intensity histogram
         """
-        # if self.frame_num % 100 == 0:
-        #     print("computing frame #", self.frame_num)
 
-        width, height, channels = self.img.shape
         intensities = list()
         bins = list(range(0,250,10))
         bins.append(255)
 
-        # for i in range(width):
-        #     for j in range(height):
-        #         intensities.append(self.get_intensity(self.img[i, j]))
         reds = self.img[:, :, 0] * 0.299
         greens = self.img[:, :, 1] * 0.587
         blues = self.img[:, :, 2] * 0.114
